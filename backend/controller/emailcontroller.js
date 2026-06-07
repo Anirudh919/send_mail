a51fbb8d8b13b5fd808a0902ee5d366fbe7e16f1
Anirudh919 <anirudhkashyap67@gmail.com>
2026-06-07 19:47:50 +0530

    add the signature

--- backend/controller/emailcontroller.js
+++ backend/controller/emailcontroller.js
@@ -3,7 +3,9 @@ const mammoth = require("mammoth");
 const { htmlToText } = require("html-to-text");
 const parseExcelFile = require("../utils/excelParsar.js");
 const path = require("path");
+const imagePath = path.join(__dirname, "../assets/signature.jpeg");
 
+console.log("Image Path:", imagePath);
 const sendBulkEmails = async (req, res) => {
   try {
     const recipientsFile = req.files?.recipients?.[0];
@@ -66,6 +68,210 @@ const sendBulkEmails = async (req, res) => {
       content: file.buffer
     }));
 
+    const signatureAttachment = {
+      filename: "signature.jpeg",
+      path: path.join(__dirname, "../assets/Sign.png"),
+      cid: "roiLogo"
+    };
+
+
+    const signatureHtml = `
+<br>
+
+<table cellpadding="0" cellspacing="0" border="0"
+       style="font-family:Arial,sans-serif;width:900px;max-width:900px;">
+
+  <tr>
+
+    <!-- Logo -->
+    <td width="280" valign="middle">
+      <img src="cid:roiLogo"
+           width="250"
+           alt="ROI Logo"
+           style="display:block;">
+    </td>
+
+    <!-- Name -->
+    <td width="280" valign="middle">
+
+      <div style="
+          font-size:28px;
+          font-weight:700;
+          color:#000;
+          line-height:1.15;
+          margin-bottom:10px;">
+          Abhishek Chauhan
+      </div>
+
+      <div style="
+          font-size:18px;
+          color:#000;
+          line-height:1.4;">
+          Business Executive
+      </div>
+
+    </td>
+
+    <!-- Contact -->
+    <td width="340" valign="middle">
+
+      <table cellpadding="0" cellspacing="0" border="0">
+
+        <tr>
+          <td width="40" style="padding-bottom:18px;">
+            <img src="cid:phoneIcon" width="28">
+          </td>
+
+          <td style="
+              font-size:18px;
+              font-weight:700;
+              color:#000;
+              padding-bottom:18px;">
+              +91 97573 78444
+          </td>
+        </tr>
+
+        <tr>
+          <td width="40" style="padding-bottom:18px;">
+            <img src="cid:mailIcon" width="28">
+          </td>
+
+          <td style="
+              font-size:18px;
+              font-weight:700;
+              color:#000;
+              padding-bottom:18px;">
+              <a href="mailto:abhishek@returnonideas.in"
+                 style="color:#000;text-decoration:none;">
+                 abhishek@returnonideas.in
+              </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td width="40">
+            <img src="cid:webIcon" width="28">
+          </td>
+
+          <td style="
+              font-size:18px;
+              font-weight:700;
+              color:#000;">
+              <a href="https://www.roiassured.com"
+                 style="color:#000;text-decoration:none;">
+                 www.roiassured.com
+              </a>
+          </td>
+        </tr>
+
+      </table>
+
+    </td>
+
+  </tr>
+
+</table>
+
+<div style="
+    width:900px;
+    background:#f4b217;
+    color:#000;
+    font-size:22px;
+    font-weight:700;
+    text-align:center;
+    padding:14px 10px;
+    margin-top:20px;">
+    Pan-India Reach | 50+ Cities Executed | 10K+ Campaigns Delivered
+</div>
+`;
+
+    // 📌 Email Signature
+    //     const signatureHtml = `
+    // <br>
+    // <table cellpadding="0" cellspacing="0" border="0" style="font-family: Arial, sans-serif;width:75%;max-width:750px;">
+    //   <tr>
+    //     <td width="260" valign="middle">
+    //       <img src="cid:roiLogo" width="220" alt="ROI Logo" style="display:block;">
+    //     </td>
+
+    //     <td width="500" valign="middle"
+    //         style="padding-left:20px;">
+    //       <div style=" font-size:20px;
+    //         font-weight:700;
+    //         color:#000;
+    //         line-height:1.2;">
+    //         Abhishek Chauhan
+    //       </div>
+
+    //       <div style="font-size:14px;
+    //         color:#000;
+    //         margin-top:8px;">
+    //         Business Executive
+    //       </div>
+    //       </td>
+
+    //       <td valign="middle" style="padding-left:30px;">
+
+    //       <table cellpadding="0" cellspacing="0">
+
+    //         <tr>
+    //           <td style="padding:8px 10px 8px 0;font-size:25px;">
+    //   <img src="cid:phoneIcon" width="18" style="vertical-align:middle;">
+    //   </td>
+    //    <td style="
+    //             font-size:20px;
+    //             font-weight:bold;
+    //             color:#000;">
+    //             +91 97573 78444
+    //           </td>
+    //         </tr>
+
+    //      <tr>
+    //           <td style="padding:8px 10px 8px 0;font-size:25px;">
+    //   <img src="cid:mailIcon" width="18" style="vertical-align:middle;">
+    //   </td>
+    //           <td style="
+    //             font-size:20px;
+    //             font-weight:bold;
+    //             color:#000;">
+    //             <a href="mailto:abhishek@returnonideas.in"
+    //                style="color:#000;text-decoration:none;">
+    //               abhishek@returnonideas.in
+    //             </a>
+    //           </td>
+    //         </tr>
+    //   <tr>
+    //           <td style="padding:8px 10px 8px 0;font-size:25px;">
+    //   <img src="cid:webIcon" width="18" style="vertical-align:middle;">
+    //    </td>
+    //           <td style="
+    //             font-size:20px;
+    //             font-weight:bold;
+    //             color:#000;">
+    //             <a href="https://www.roiassured.com"
+    //                style="color:#000;text-decoration:none;">
+    //               www.roiassured.com
+    //             </a>
+    //           </td>
+    //         </tr>
+    //         </table>
+    //     </td>
+    //   </tr>
+    // </table>
+
+    // <div style="
+    //  background:#f4b217;
+    //   color:#000;
+    //   font-weight:700;
+    //   font-size:24px;
+    //   text-align:center;
+    //   padding:12px;
+    //   margin-top:15px;
+    // ">
+    // Pan-India Reach | 50+ Cities Executed | 10K+ Campaigns Delivered
+    // </div>
+    // `;
+
     let success = 0;
     let failed = [];
 
@@ -81,17 +287,25 @@ const sendBulkEmails = async (req, res) => {
           from: `${process.env.FROM_NAME} <${process.env.EMAIL_USER}>`,
           to: user.email,
           subject,
-          html: personalizedHtml + `
-    <br/><br/>
-    <img src="cid:signatureImage" style="width:500px; max-width:100%;" />
-  `,
+          html: personalizedHtml + signatureHtml,
           text: htmlToText(personalizedHtml),
           attachments: [
             ...attachments,
+            signatureAttachment,
             {
-              filename: "signature.jpg",
-              path: path.join(__dirname, "../assets/signature.jpeg"),
-              cid: "signatureImage"
+              filename: "phone.png",
+              path: path.join(__dirname, "../assets/phone.png"),
+              cid: "phoneIcon"
+            },
+            {
+              filename: "mail.png",
+              path: path.join(__dirname, "../assets/email.png"),
+              cid: "mailIcon"
+            },
+            {
+              filename: "web.png",
+              path: path.join(__dirname, "../assets/network.png"),
+              cid: "webIcon"
             }
           ]
         });
